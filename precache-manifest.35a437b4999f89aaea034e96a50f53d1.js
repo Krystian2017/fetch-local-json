93827620b6e85b845c02c1a7e0e2ff6aa41d6429
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 17:18:51 +0200

    Updates

---
+++ precache-manifest.35a437b4999f89aaea034e96a50f53d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6a00a7a404601608f7e",
+    "url": "/Fetch-local-json/static/js/runtime~main.f6a00a7a.js"
+  },
+  {
+    "revision": "e14a86e090b7685ee345",
+    "url": "/Fetch-local-json/static/js/main.e14a86e0.chunk.js"
+  },
+  {
+    "revision": "2eb1efbf424eb56e824f",
+    "url": "/Fetch-local-json/static/js/1.2eb1efbf.chunk.js"
+  },
+  {
+    "revision": "e14a86e090b7685ee345",
+    "url": "/Fetch-local-json/static/css/main.bb79e224.chunk.css"
+  },
+  {
+    "revision": "b0d88a51233f17443ea37facc25cd88c",
+    "url": "/Fetch-local-json/index.html"
+  }
+];
